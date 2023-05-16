cf4783e345eb119fe19a425c30d29d3789f712b0
이해송 <79689822+pinetree2@users.noreply.github.com>
2023-05-17 00:54:05 +0900

    Update Board.java
    어노테이션추가

--- entity/Board.java
+++ entity/Board.java
@@ -2,8 +2,13 @@ package com.example.board.entity;
 
 import jakarta.persistence.Entity;
 
-//테이블을 의미하는 어노테이션
+//DB 에 있는 테이블을 의미하는 어노테이션
 @Entity
 public class Board {
     //테이블을 토대로 작성해주시면됩니다.
+    
+    @Id //해당 클래스의 주요 식별자(primary key)를 나타내는 어노테이션
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//JPA에서 기본 키를 자동으로 생성할 때 사용하는 방법 중 하나
+    
+    
 }
