12423e5a45e4b29f81b277cf228282d0d4f8cd56
이해송 <79689822+pinetree2@users.noreply.github.com>
2023-05-17 00:29:36 +0900

    Update BoardController.java
    write pro method 추가

--- controller/BoardController.java
+++ controller/BoardController.java
@@ -2,6 +2,7 @@ package com.example.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,13 +14,21 @@ public class BoardController {
      *     public String main(){
      *         return "Hello World";
      *     }
+     *
      * */
-  
     //GetMapping : 어떤 url 로 접근할 것인지 지정해주는 어노테이션
-    //localhost:8090/board/wirte 주소로 접속하면 board write.html 페이지 보여주는 설정
+
+    //localhost:8090/board/wirte 주소로 접속하면 board write 페이지 보여주는 설정
     @GetMapping("/board/write")
     public String boardwriteForm(){
         return "boardwrite";
     }
 
+    //boardwrite.html 에서의 요청이 넘어왔는지 확인하는 메소드
+    @PostMapping("/board/writepro")
+    public String boardWritePro(String title, String content){
+
+        //구현하시면됩니다.
+        return "";
+    }
 }
