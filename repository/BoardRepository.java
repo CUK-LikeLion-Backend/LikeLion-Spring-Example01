1c7af1093cd136c9095ec0acfc0d0269319d6f39
이해송 <79689822+pinetree2@users.noreply.github.com>
2023-05-17 13:01:42 +0900

    Update BoardRepository.java

--- repository/BoardRepository.java
+++ repository/BoardRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Integer> {
 
-
+//페이징 기능시에 필요함 
+  //위 기능 구현안하면 그냥 복붙만해주세요~
 }
